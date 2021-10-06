6b2a38400c8b1651365ca558a2ebba52389fd3f9
Sting Nguyen <nguyenminhtienit@gmail.com>
2021-10-06 23:09:36 +0700

    nguyenminhtienitvn

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-base" ../base && \
+  docker tag "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-base" nguyenminhtienitvn/meteord:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-onbuild" nguyenminhtienitvn/meteord:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-devbuild" nguyenminhtienitvn/meteord:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "nguyenminhtienitvn/meteord:node-${NODE_VERSION}-binbuild" nguyenminhtienitvn/meteord:binbuild
